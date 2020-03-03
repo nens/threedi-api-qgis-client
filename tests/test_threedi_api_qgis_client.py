671e9f7df1c557684577a72485fa4f459eae2f40
lukasz.debek <lukasz.debek@gigaclear.com>
2020-03-03 18:35:45 +0100

    Working on handling 3Di API calls...

--- tests/test_threedi_api_qgis_client.py
+++ tests/test_threedi_api_qgis_client.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Tests for `threedi_api_qgis_client` package."""
-
 import pytest
+import datetime
+from mock import Mock, patch
+from openapi_client import Simulation
+from threedi_api_qgis_client.api_calls.threedi_calls import ThreediCalls, get_api_client
 
 
-from threedi_api_qgis_client import threedi_api_qgis_client
+@patch('openapi_client.RepositoriesApi.repositories_list')
+def test_fetch_repositories(mock_repositories_list):
+    mock_repositories_list.return_value = Mock(results=['result1', 'result2'])
+    api = get_api_client('dummy_api', 'dummy_username', 'dummy_password', testing=True)
+    tc = ThreediCalls(api)
+    repos = tc.fetch_repositories()
+    assert repos == ['result1', 'result2']
+    #assert mock_repositories_list.assert_called()
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+@patch('openapi_client.SimulationsApi.simulations_create')
+def test_new_simulation(mock_simulations_create):
+    mock_simulations_create.side_effect = lambda x: x
+    api = get_api_client('dummy_api', 'dummy_username', 'dummy_password', testing=True)
+    tc = ThreediCalls(api)
+    data = {
+        "name": "qgis client test run",
+        "threedimodel": "14",
+        "organisation": "cb0347bf57f7450984c4b1d27271c90f",
+        "start_datetime":  datetime.datetime.utcnow(),
+        "duration": 72000
+    }
+    sim = tc.new_simulation(**data)
+    assert isinstance(sim, Simulation)
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
--- threedi_api_qgis_client/api_calls/threedi_calls.py
+++ threedi_api_qgis_client/api_calls/threedi_calls.py
@@ -1,91 +1,98 @@
 import os
 import datetime
+from typing import List, Dict
 from threedi_api_client import ThreediApiClient
 from openapi_client.exceptions import ApiException
-from openapi_client import (RepositoriesApi, SimulationsApi, UsageApi, Simulation, Action, Progress, InpyVersionsApi,
-                            RevisionsApi, InlineResponse20050, InlineResponse2008, InlineResponse2002,
-                            InlineResponse2006, InlineResponse2005)
+from openapi_client import (ApiClient, RepositoriesApi, Repository, SimulationsApi, Simulation, UsageApi, Usage, Action,
+                            Progress, RevisionsApi, Revision, ThreediModel, ConstantRain, TimeseriesRain)
+
+
+def get_api_client(api_host: str, api_username: str, api_password: str, testing: bool = False) -> ApiClient:
+    """Setup open_api client using username and password."""
+    os.environ["API_HOST"] = api_host
+    os.environ["API_USERNAME"] = api_username
+    os.environ["API_PASSWORD"] = api_password
+    if testing is True:
+        api_client = ApiClient()
+    else:
+        api_client = ThreediApiClient()
+    return api_client
 
 
 class ThreediCalls:
     """Class to do all the communication with the 3Di API."""
+    def __init__(self, api_client: ApiClient) -> None:
+        self.api_client = api_client
 
-    def __init__(self, api_host, api_username, api_password):
-        """
-        Setup open_api client using username and password.
-        """
-        os.environ["API_HOST"] = api_host
-        os.environ["API_USERNAME"] = api_username
-        os.environ["API_PASSWORD"] = api_password
-        self.api_host = api_host
-        self.api_username = api_username
-        self.api_client = ThreediApiClient()
-        self._config = self.api_client.configuration
-        self._token = self._config.access_token
-
-    def fetch_repositories(self) -> InlineResponse2005:
+    def fetch_repositories(self) -> List[Repository]:
         """Fetch all repositories available for current user."""
         api = RepositoriesApi(self.api_client)
-        repositories_list = api.repositories_list()
+        repositories_list = api.repositories_list().results
         return repositories_list
 
-    def fetch_simulations(self) -> InlineResponse2008:
+    def fetch_simulations(self) -> List[Simulation]:
         """Fetch all simulations available for current user."""
         api = SimulationsApi(self.api_client)
-        simulations_list = api.simulations_list()
+        simulations_list = api.simulations_list().results
         return simulations_list
 
-    def fetch_inpy_versions(self) -> InlineResponse2002:
-        """Fetch all Inpy Versions available for current user."""
-        api = InpyVersionsApi(self.api_client)
-        inpy_versions_list = api.inpy_versions_list()
-        return inpy_versions_list
-
-    def fetch_revisions(self) -> InlineResponse2006:
+    def fetch_revisions(self) -> List[Revision]:
         """Fetch all Revisions available for current user."""
         api = RevisionsApi(self.api_client)
-        revisions_list = api.revisions_list()
+        revisions_list = api.revisions_list().results
         return revisions_list
 
-    def fetch_revision_3di_models(self, rev_id) -> list:
+    def fetch_revision_3di_models(self, rev_id: int) -> List[ThreediModel]:
         """Fetch all 3Di models belonging to given Revision."""
         api = RevisionsApi(self.api_client)
         revision_models_list = api.revisions_threedimodels(rev_id)
         return revision_models_list
 
-    def fetch_usage(self) -> InlineResponse20050:
+    def fetch_usage(self) -> List[Usage]:
         """Fetch overview of ran simulations."""
         api = UsageApi(self.api_client)
-        usage_list = api.usage_list()
+        usage_list = api.usage_list().results
         return usage_list
 
     def new_simulation(self, **simulation_data) -> Simulation:
         """Create a new Simulation."""
         api = SimulationsApi(self.api_client)
         sim = Simulation(**simulation_data)
-        res = api.simulations_create(sim)
-        return res
+        new_sim = api.simulations_create(sim)
+        return new_sim
 
-    def make_action_on_simulation(self, simulation_pk, **action_data) -> Action:
+    def make_action_on_simulation(self, simulation_pk: int, **action_data) -> Action:
         """Make an action on 'simulation_pk' simulation."""
         api = SimulationsApi(self.api_client)
-        res = api.simulations_actions_create(str(simulation_pk), action_data)
-        return res
+        action = api.simulations_actions_create(str(simulation_pk), action_data)
+        return action
 
-    def simulations_progress(self, simulation_pk) -> Progress:
+    def add_constant_precipitation(self, simulation_pk: int, **rain_data) -> ConstantRain:
+        """Add ConstantRain to the given simulation."""
+        api = SimulationsApi(self.api_client)
+        constant_rain = api.simulations_events_rain_constant_create(str(simulation_pk), rain_data)
+        return constant_rain
+
+    def add_custom_precipitation(self, simulation_pk: int, **rain_data) -> TimeseriesRain:
+        """Add TimeseriesRain to the given simulation."""
+        api = SimulationsApi(self.api_client)
+        time_series_rain = api.simulations_events_rain_timeseries_create((str(simulation_pk)), rain_data)
+        return time_series_rain
+
+    def simulations_progress(self, simulation_pk: int) -> Progress:
         """Get a given simulation progress. Available only if simulation was already started."""
         api = SimulationsApi(self.api_client)
         simulations_progress = api.simulations_progress_list(str(simulation_pk))
         return simulations_progress
 
-    def all_simulations_progress(self) -> dict:
+    def all_simulations_progress(self) -> Dict[str, Dict[str, int]]:
         """Get all simulations progresses."""
         api = SimulationsApi(self.api_client)
         progresses = {}
-        for sim in self.fetch_simulations().results:
-            spk = sim.id
+        for sim in self.fetch_simulations():
+            spk = str(sim.id)
             try:
-                sim_progress = api.simulations_progress_list(str(spk))
+                sim_progress = api.simulations_progress_list(spk)
             except ApiException:
                 sim_progress = {'percentage': 0, 'time': 0}
             progresses[spk] = sim_progress
@@ -97,21 +104,27 @@ if __name__ == "__main__":
     API_HOST = "https://api.3di.live/v3.0"
     API_USERNAME = sys.argv[1]
     API_PASSWORD = sys.argv[2]
-    tc = ThreediCalls(API_HOST, API_USERNAME, API_PASSWORD)
+    aclient = get_api_client(API_HOST, API_USERNAME, API_PASSWORD)
+    tc = ThreediCalls(aclient)
     repos = tc.fetch_repositories()
     sims = tc.fetch_simulations()
     print(repos)
     print(sims)
     #print(tc.make_action_on_simulation(1203, name="start"))
-
-    data = {
+    sim_data = {
         "name": "qgis client test run 4",
         "threedimodel": "14",
         "organisation": "cb0347bf57f7450984c4b1d27271c90f",
         "start_datetime":  datetime.datetime.utcnow(),
         "duration": 72000
     }
-    #print(tc.new_simulation(**data))
+    #print(tc.new_simulation(**sim_data))
     print(tc.all_simulations_progress())
     print(tc.fetch_revisions())
     print(tc.fetch_revision_3di_models(8))
+    constant_rain_data = {
+        'durationnn': 5000,
+        'offset': 60,
+        'units': 'm/s',
+        'value': 0.0006}
+    print(tc.add_constant_precipitation(1203, **constant_rain_data))
