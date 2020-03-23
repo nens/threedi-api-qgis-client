14ce92f216758486d2872f9d3c6190b5d693440d
lukasz.debek <lukasz.debek@gigaclear.com>
2020-03-23 09:03:43 +0100

    Fixed custom imports.

--- threedi_api_qgis_client/deps/custom_imports.py
+++ threedi_api_qgis_client/deps/custom_imports.py
@@ -1,17 +1,28 @@
 import os
 import sys
-try:
-    import pyqtgraph as pg
-except ImportError:
-    main_dir = os.path.dirname(os.path.abspath(__file__))
-    deps_path = os.path.join(main_dir, pyqtgraph-0.10.0-py3-none-any.whl")
-    sys.path.append(deps_path)
-    import pyqtgraph as pg
 
-try:
-    from dateutil.relativedelta import relativedelta
-except ImportError:
+
+def patch_wheel_imports():
+    """
+    Function that tests if extra modules are installed.
+    If modules are not available then it will add missing modules wheels to the Python path.
+    """
     main_dir = os.path.dirname(os.path.abspath(__file__))
-    deps_path = os.path.join(main_dir, "python_dateutil-2.8.1-py2.py3-none-any.whl")
-    sys.path.append(deps_path)
-    from dateutil.relativedelta import relativedelta
+    try:
+        import pyqtgraph
+    except ImportError:
+        deps_path = os.path.join(main_dir, "pyqtgraph-0.10.0-py3-none-any.whl")
+        sys.path.append(deps_path)
+
+    try:
+        import dateutil
+    except ImportError:
+        deps_path = os.path.join(main_dir, "python_dateutil-2.8.1-py2.py3-none-any.whl")
+        sys.path.append(deps_path)
+
+    try:
+        import threedi_api_client
+        import openapi_client
+    except ImportError:
+        deps_path = os.path.join(main_dir, "threedi_api_client-3.0b10-py2.py3-none-any.whl")
+        sys.path.append(deps_path)
