cc057df6c94c3cdc7b0de4c5cc49921138b5f343
PachKosti <razumichin1@gmail.com>
2020-05-13 14:53:32 +0200

    added upload dialog (closes #46)

---
+++ threedi_api_qgis_client/widgets/upload.py
@@ -0,0 +1,20 @@
+import os
+from qgis.PyQt import uic
+
+
+base_dir = os.path.dirname(os.path.dirname(__file__))
+uicls_log, basecls_log = uic.loadUiType(os.path.join(base_dir, 'ui', 'upload_dialog.ui'))
+
+
+class UploadDialog(uicls_log, basecls_log):
+    def __init__(self, parent_dock, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.parent_dock = parent_dock
+        self.communication = self.parent_dock.communication
+
+        self.pb_cancel.clicked.connect(self.close)
+        self.resize(600, 350)
+
+    # def close(self):
+    #     self.close()
