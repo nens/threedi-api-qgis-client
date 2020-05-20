20efedc9a87bf929922ef8c5b548daca7197c2a3
Richard Boon <richard.boon@nelen-schuurmans.nl>
2020-05-20 09:34:51 +0200

    Update upload-artifact.sh
    Co-authored-by: Reinout van Rees <reinout@vanrees.org>

--- upload-artifact.sh
+++ upload-artifact.sh
@@ -7,7 +7,7 @@ VERSION=$(grep "^version" ./threedi_api_qgis_client/metadata.txt | cut -d= -f2)
 # ARTIFACTS_KEY should be set as env variable in the travis UI.
 # TRAVIS_BRANCH is set automatically by travis
 ARTIFACT=threedi_api_qgis_client_${VERSION}.zip
-PROJECT=threedi_api_qgis_client
+PROJECT=threedi-api-qgis-client
 
 # Rename generated threedi_api_qgis_client.zip to include version number.
 cp threedi_api_qgis_client.zip ${ARTIFACT}
