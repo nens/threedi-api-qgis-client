d4923b4f79bb3de26e61f54db9dc5fe4a1552370
RichardB9 <richardboon1991@gmail.com>
2020-05-25 09:03:12 +0200

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nens/threedi-api-qgis-client',
-    version='1.0.0',
+    version='1.0.1.dev0',
     zip_safe=False,
 )
