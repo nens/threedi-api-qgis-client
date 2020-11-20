29b3185a63e5b73f1fd414977e59b5440e07c14f
lukasz.debek <lukasz.debek@lutraconsulting.co.uk>
2020-11-20 10:10:41 +0100

    Minor UI updates.

--- zip_plugin.py
+++ zip_plugin.py
@@ -7,7 +7,7 @@ import re
 
 def get_version(directory):
     metadata = os.path.join(directory, "metadata.txt")
-    reg = '\nversion=(.+)\n'
+    reg = "\nversion=(.+)\n"
     version = ""
     with open(metadata, "r") as f:
         m0 = re.search(reg, f.read())
@@ -17,12 +17,12 @@ def get_version(directory):
 
 
 if __name__ == "__main__":
-    print('ZIPPING PLUGIN STARTED')
+    print("ZIPPING PLUGIN STARTED")
     this_dir = os.path.dirname(os.path.realpath(__file__))
     plugin_dirname = "threedi_api_qgis_client"
     plugin_path = os.path.join(this_dir, plugin_dirname)
     plugin_version = get_version(plugin_path)
     zip_filename = f"{plugin_dirname}.{plugin_version}"
     plugin_zip_path = os.path.join(this_dir, zip_filename)
-    shutil.make_archive(plugin_zip_path, 'zip', this_dir, plugin_dirname)
-    print('ZIPPING PLUGIN FINISHED')
+    shutil.make_archive(plugin_zip_path, "zip", this_dir, plugin_dirname)
+    print("ZIPPING PLUGIN FINISHED")
