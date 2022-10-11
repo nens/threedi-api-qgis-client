8f03f78289a92aa796007d5e9a77e8b6c66009c6
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2022-10-11 21:55:43 +0200

    Use constant for max_angle_1d_advection
    instead of math.radians(90) because it turns out that the theedi_modelchecker requires this value to be a bit below math.radians(90)

--- threedi_models_and_simulations/widgets/new_schematisation_wizard.py
+++ threedi_models_and_simulations/widgets/new_schematisation_wizard.py
@@ -1,6 +1,5 @@
 # 3Di Models and Simulations for QGIS, licensed under GPLv2 or (at your option) any later version
 # Copyright (C) 2022 by Lutra Consulting for 3Di Water Management
-import math
 import os
 import shutil
 import time
@@ -194,7 +193,7 @@ class SchematisationSettingsWidget(uicls_schema_settings_page, basecls_schema_se
             "max_interception_file": None,
             "kmax": 1,
             "manhole_storage_area": None,
-            "max_angle_1d_advection": math.radians(90),
+            "max_angle_1d_advection": 1.570795,
             "maximum_sim_time_step": None,
             "maximum_table_step_size": None,
             "minimum_sim_time_step": 0.01,
