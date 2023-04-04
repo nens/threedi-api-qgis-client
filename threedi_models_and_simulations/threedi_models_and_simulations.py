4999c75a7f81615a40c818bc0e8b50ce158c9356
lukasz.debek <lukasz.debek@lutraconsulting.co.uk>
2023-04-04 11:18:55 +0200

    Removed translation methods.

--- threedi_models_and_simulations/threedi_models_and_simulations.py
+++ threedi_models_and_simulations/threedi_models_and_simulations.py
@@ -21,13 +21,7 @@ class ThreediModelsAndSimulations:
     """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
-        """Constructor.
-
-        :param iface: An interface instance that will be passed to this class
-            which provides the hook by which you can manipulate the QGIS
-            application at run time.
-        :type iface: QgsInterface
-        """
+        """Constructor."""
         # Save reference to the QGIS interface
         self.iface = iface
 
@@ -35,37 +29,14 @@ class ThreediModelsAndSimulations:
         self.plugin_dir = os.path.dirname(__file__)
         # initialize plugin settings
         self.plugin_settings = SettingsDialog(self.iface)
-        # initialize locale
-        locale = QSettings().value("locale/userLocale")[0:2]
-        locale_path = os.path.join(self.plugin_dir, "i18n", f"ThreediModelsAndSimulations_{locale}.qm")
-
-        if os.path.exists(locale_path):
-            self.translator = QTranslator()
-            self.translator.load(locale_path)
-            QCoreApplication.installTranslator(self.translator)
 
         # Declare instance attributes
         self.actions = []
-        self.menu = self.tr("&3Di Models and Simulations")
+        self.menu = "3Di Models and Simulations"
         self.toolbar = self.iface.addToolBar("ThreediModelsAndSimulations")
         self.toolbar.setObjectName("ThreediModelsAndSimulations")
         self.pluginIsActive = False
         self.dockwidget = None
-
-    # noinspection PyMethodMayBeStatic
-    def tr(self, message):
-        """Get the translation for a string using Qt translation API.
-
-        We implement this ourselves since we do not inherit QObject.
-
-        :param message: String for translation.
-        :type message: str, QString
-
-        :returns: Translated version of message.
-        :rtype: QString
-        """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-        return QCoreApplication.translate("ThreediModelsAndSimulations", message)
 
     def add_action(
         self,
@@ -79,44 +50,7 @@ class ThreediModelsAndSimulations:
         whats_this=None,
         parent=None,
     ):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
+        """Add a toolbar icon to the toolbar."""
 
         icon = QIcon(icon_path)
         action = QAction(icon, text, parent)
@@ -142,19 +76,17 @@ class ThreediModelsAndSimulations:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
         icon_path = os.path.join(os.path.dirname(__file__), "icon.png")
-        self.add_action(
-            icon_path, text=self.tr("3Di Models and Simulations"), callback=self.run, parent=self.iface.mainWindow()
-        )
+        self.add_action(icon_path, text="3Di Models and Simulations", callback=self.run, parent=self.iface.mainWindow())
         self.add_action(
             icon_path,
-            text=self.tr("Settings"),
+            text="Settings",
             callback=self.settings,
             parent=self.iface.mainWindow(),
             add_to_toolbar=False,
         )
 
     def onClosePlugin(self):
-        """Cleanup necessary items here when plugin dockwidget is closed"""
+        """Cleanup necessary items here when plugin dockwidget is closed."""
         # disconnects
         self.dockwidget.closingPlugin.disconnect(self.onClosePlugin)
         self.pluginIsActive = False
@@ -162,7 +94,7 @@ class ThreediModelsAndSimulations:
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
         for action in self.actions:
-            self.iface.removePluginMenu(self.tr("&3Di Models and Simulations"), action)
+            self.iface.removePluginMenu("3Di Models and Simulations", action)
             self.iface.removeToolBarIcon(action)
         # remove the toolbar
         del self.toolbar
